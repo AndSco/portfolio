09bfadff76ac5d8a0bdd1fcb152c611f833e405f
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-06-25 17:05:01 +0200

    issues locstorage

--- src/components/UIComponents/ThemeSwitch.js
+++ src/components/UIComponents/ThemeSwitch.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../styles/themeSwitch.css";
 
+
+//defaultChecked={theme === "light" ? false : true}
 const ThemeSwitch = ({ changeTheme, theme }) => {
   return (
     <section>
       <div className="toggle-btn" id="_1st-toggle-btn">
-        <input type="checkbox" onChange={changeTheme} defaultChecked={theme === "light" ? false : true} />
+        <input type="checkbox" onChange={changeTheme} />
         <span></span>
       </div>
     </section>
