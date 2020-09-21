c46a7508cbe6e3da04340ba55df6f8dac3280de1
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-09-21 11:56:10 +0200

    edit seakats as prototype

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -50,8 +50,15 @@ export default [
   {
     title: "Kayaking mobile app",
     description:
-      "React Native application built for the Sea Kayak club in Malta. Users can check in when going out for a paddle, specifying their itinerary & estimated time of arrival. If the kayaker does not check out by his/her ETA, all subscribers receive an alert notification. The app also serves as a logbook of the club's activities, a repository of kayaking trips, a source of technical references and handy practical info (e.g. nautical units conversion, interpretation of buoyage and light signals).",
-    stack: ["React Native", "NodeJS", "API Creation", "Redux", "MongoDB"],
+      "Working prototype of React Native application built for the Sea Kayak club in Malta. Users can check in when going out for a paddle, specifying their itinerary & estimated time of arrival. If the kayaker does not check out by his/her ETA, all subscribers receive an alert notification. The app also serves as a logbook of the club's activities, a repository of kayaking trips, a source of technical references and handy practical info (e.g. nautical units conversion, interpretation of buoyage and light signals).",
+    stack: [
+      "React Native",
+      "Expo",
+      "NodeJS",
+      "API Creation",
+      "Redux",
+      "MongoDB",
+    ],
     picName: "seakats.jpg",
     demoUrl: "https://www.youtube.com/embed/pSsuKNdnCoA",
     url: "NA",
