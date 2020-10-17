c4e1664db57054bfce81b59694f40447c333d47b
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-10-17 13:32:20 +0200

    try open graph preview

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import ogImage from "../images/og-image.png"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -43,6 +44,13 @@ function SEO({ description, lang, meta, title }) {
         {
           property: `og:type`,
           content: `website`,
+        },
+        {
+          property: `og:image`,
+          // You should ideally replace the hardcoded URL below with a value you set
+          // in your gatsby-config.js file.  And import all shared site metadata into
+          // this component with the useStaticQuery hook.
+          content: `https://www.andreascorcia.com/${ogImage}`,
         },
         {
           name: `twitter:card`,
