7fb2dca88bb101e935f01b0bd65bb78f32f25758
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-11-01 12:43:24 +0100

    fix typo in weather app

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -98,7 +98,7 @@ export default [
     title: "FamilyWeather+",
     description:
       "The usual weather project... with a twist! Having my family spread in different European cities, this progressive web app gives an overview of the weather for each of these locations, sorting them according to various parameters. Are your dear ones living somewhere else? Or maybe you just want to compare weather in different locations ahead of your weekend break? Create and save your own dashboard! The data is from the Open Weather Map API.",
-    stack: ["React", "NodeJS", "API consumption", "PWA"],
+    stack: ["React", "API consumption", "PWA"],
     picName: "familyWeather.png",
     demoUrl: "https://www.youtube.com/embed/kpToaEfVOd4",
     url: "https://familyweather.herokuapp.com/",
